0c1502ab32d205cf4812664b9a6ae0e8c718d2f8
Deep Tailor <deeptailor@gmail.com>
2016-11-21 11:03:47 -0800

    frontend setup

---
+++ frontend/ziva.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('main');
+    ReactDOM.render(<h1>Welcome to Ziva</h1>, root);
+});
